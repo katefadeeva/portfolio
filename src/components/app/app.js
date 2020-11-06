dabc2efe30a7eea6aa86b9eec193ba3201d96302
katesfadeeva <katesfadeeva@gmail.com>
2020-11-06 18:55:05 +0300

    init: start develop portfolio

---
+++ src/components/app/app.js
@@ -0,0 +1,14 @@
+import React, { Component }from 'react';
+import './app.scss'
+
+export default class App extends Component {
+
+  render() {
+
+    return (
+        <React.Fragment>
+          <p>Hello</p>
+        </React.Fragment>
+    )
+  }
+}
